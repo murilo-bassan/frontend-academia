d44624a000ea8a35cafdf6b8b1606471b7a8963a
murilo_bassan <murilogabassan@gmail.com>
2025-09-30 08:35:16 -0400

    feat: navbar

---
+++ src/app/shared/navbar/navbar.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './navbar.html',
+  styleUrls: ['./navbar.scss']
+})
+export class Navbar { }
