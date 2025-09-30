956207e8ea04d995608a9b7bdeab9bceb2721901
murilo_bassan <murilogabassan@gmail.com>
2025-09-30 08:32:40 -0400

    feat: routing, style, adicionando navbar à toda a aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard';
+import { ListaAlunos } from './features/alunos/lista-alunos';
+import { FormAluno } from './features/alunos/form-aluno';
+import { ListaPagamentos } from './features/pagamentos/lista-pagamentos';
+import { FormPagamento } from './features/pagamentos/form-pagamento';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Dashboard },
+    { path: 'alunos', component: ListaAlunos },
+    { path: 'alunos/novo', component: FormAluno },
+    { path: 'alunos/:id/editar', component: FormAluno },
+    { path: 'alunos/:id/pagamentos', component: ListaPagamentos },
+    { path: 'alunos/:id/pagamentos/novo', component: FormPagamento },
+    { path: 'pagamentos/:id/editar', component: FormPagamento },
+    { path: '**', redirectTo: '' }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './shared/navbar/navbar';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
